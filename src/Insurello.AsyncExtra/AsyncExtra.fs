6404f0ddcfeefb054266331ae311318948b88b64
Kristian Lundström <kristian.lundstrom@insurello.se>
2020-10-13 10:28:03 +0200

    wip

--- src/Insurello.AsyncExtra/AsyncExtra.fs
+++ src/Insurello.AsyncExtra/AsyncExtra.fs
@@ -30,6 +30,15 @@ module AsyncResult =
                 | Choice1Of2 response -> Ok response
                 | Choice2Of2 exn -> Error exn.Message)
 
+    let fromTeeTask: System.Threading.Tasks.Task -> AsyncResult<'x, string> =
+        fun task ->
+            task
+            |> Async.AwaitTask
+            |> Async.Catch
+            |> Async.map (function
+                | Choice1Of2 response -> Ok response
+                | Choice2Of2 exn -> Error exn.Message)
+
     let map: ('x -> 'y) -> AsyncResult<'x, 'err> -> AsyncResult<'y, 'err> =
         fun f asyncResultX -> Async.map (Result.map f) asyncResultX
 
