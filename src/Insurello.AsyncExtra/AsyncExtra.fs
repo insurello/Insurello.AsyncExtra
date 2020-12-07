f5348dfad7c92c16c2e12473d422d0250843e069
Jamil Maqdis Anton <jamil.maqdis-anton@insurello.se>
2020-12-07 20:36:39 +0100

    Add some functions, and AsyncResultBuilder with extensions.

--- src/Insurello.AsyncExtra/AsyncExtra.fs
+++ src/Insurello.AsyncExtra/AsyncExtra.fs
@@ -67,3 +67,88 @@ module AsyncResult =
             asyncs
             |> List.fold folder (fromResult (Ok []))
             |> map List.rev
+
+    let singleton: 'value -> AsyncResult<'value, 'e> = fun x -> Async.singleton (Ok x)
+
+    let fromAsync: Async<'x> -> AsyncResult<'x, string> =
+        fun async ->
+            async
+            |> Async.Catch
+            |> Async.map (function
+                | Choice1Of2 response -> Ok response
+                | Choice2Of2 exn -> Error exn.Message)
+
+    let andMap: AsyncResult<'a, 'e> -> AsyncResult<('a -> 'b), 'e> -> AsyncResult<'b, 'e> =
+        fun asyncResult f ->
+            async {
+                let! execF = Async.StartChild f
+
+                let! execAsyncResult = Async.StartChild asyncResult
+
+                let! f' = execF
+
+                let! asyncResult' = execAsyncResult
+
+                return match f', asyncResult' with
+                       | Ok fOk, Ok aROk -> Ok(fOk aROk)
+                       | Error e, _ -> Error e
+                       | _, Error e -> Error e
+            }
+
+    let zip: AsyncResult<'a, 'e> -> AsyncResult<'b, 'e> -> AsyncResult<('a * 'b), 'e> =
+        fun async1 async2 ->
+            (fun a b -> a, b)
+            |> singleton
+            |> andMap async1
+            |> andMap async2
+
+[<AutoOpen>]
+module AsyncResultCE =
+    type AsyncResultBuilder() =
+        member _.Return(value: 'a): AsyncResult<'a, 'e> = AsyncResult.singleton value
+
+        member _.ReturnFrom(asyncResult: AsyncResult<'a, 'e>): AsyncResult<'a, 'e> = asyncResult
+
+        member _.Zero(): AsyncResult<unit, 'e> = AsyncResult.singleton ()
+
+        member _.Bind(asyncResult: AsyncResult<'a, 'e>, f: 'a -> AsyncResult<'b, 'e>): AsyncResult<'b, 'e> =
+            AsyncResult.bind f asyncResult
+
+        member _.Delay(f: unit -> AsyncResult<'a, 'e>): AsyncResult<'a, 'e> = async.Delay f
+
+        member _.Combine(unitAsyncResult: AsyncResult<unit, 'e>, asyncResult: AsyncResult<'a, 'e>)
+                         : AsyncResult<'a, 'e> =
+            AsyncResult.bind (fun () -> asyncResult) unitAsyncResult
+
+        member _.TryWith(asyncResult: AsyncResult<'a, 'e>, f: exn -> AsyncResult<'a, 'e>): AsyncResult<'a, 'e> =
+            async.TryWith(asyncResult, f)
+
+        member _.TryFinally(asyncResult: AsyncResult<'a, 'e>, f: unit -> unit): AsyncResult<'a, 'e> =
+            async.TryFinally(asyncResult, f)
+
+        member _.Using(disposable: 'a :> System.IDisposable, f: 'a -> AsyncResult<'b, 'e>): AsyncResult<'b, 'e> =
+            async.Using(disposable, f)
+
+        member _.BindReturn(asyncResult: AsyncResult<'a, 'e>, f: 'a -> 'b): AsyncResult<'b, 'e> =
+            AsyncResult.map f asyncResult
+
+        member _.MergeSources(asyncResult1: AsyncResult<'a, 'e>, asyncResult2: AsyncResult<'b, 'e>)
+                              : AsyncResult<'a * 'b, 'e> =
+            AsyncResult.zip asyncResult1 asyncResult2
+
+        member inline _.Source(asyncResult: AsyncResult<'a, 'e>): AsyncResult<'a, 'e> = asyncResult
+
+    let asyncResult = AsyncResultBuilder()
+
+[<AutoOpen>]
+module AsyncResultCEExtensions =
+    type AsyncResultBuilder with
+        member inline _.Source(asyncOp: Async<'a>): AsyncResult<'a, string> = AsyncResult.fromAsync asyncOp
+
+        member inline _.Source(result: Result<'a, 'e>): AsyncResult<'a, 'e> = AsyncResult.fromResult result
+
+        member inline _.Source(task: System.Threading.Tasks.Task<'a>): AsyncResult<'a, string> =
+            AsyncResult.fromTask (fun () -> task)
+
+        member inline _.Source(unitTask: System.Threading.Tasks.Task): AsyncResult<unit, string> =
+            AsyncResult.fromUnitTask (fun () -> unitTask)
